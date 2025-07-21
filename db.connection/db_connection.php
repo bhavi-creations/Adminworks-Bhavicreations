83a77360a9d405596a13d3f70b14889e22570321
manimalladi <manimalladi05@gmail.com>
2025-07-21 12:28:37 +0530

    changes

--- db.connection/db_connection.php
+++ db.connection/db_connection.php
@@ -7,9 +7,9 @@ if ($_SERVER['SERVER_NAME'] == 'localhost') {
     $password = "";
     $dbname = "bhavicreations_db";
 } else {
-    $username = "bhavicreationspvtltd";
-    $password = "qbWkuINh3wc8P9z";
-    $dbname = "bhavicreationspvtltd";
+    $username = "bhavicreations";
+    $password = "d8Az75YlgmyBnVM";
+    $dbname = "bhavicreations";
    
     
 }
